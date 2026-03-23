87348c053091d860840ce7cb98de9163fc8e1e8c
RobertJohnsonSequencerPro <johnson.robert.e@gmail.com>
2026-03-22 18:03:48 -0600

    Shared components and CRUD pages

--- tests/TeamStrategyAndTasks.Integration.Tests/obj/Debug/net8.0/TeamStrategyAndTasks.Integration.Tests.AssemblyInfo.cs
+++ tests/TeamStrategyAndTasks.Integration.Tests/obj/Debug/net8.0/TeamStrategyAndTasks.Integration.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamStrategyAndTasks.Integration.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5fd2bfd6480b57bf6f614821a15e3f3bd6cc30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c3d6579d8d003f0ffb00a91079bb71352d522d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamStrategyAndTasks.Integration.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamStrategyAndTasks.Integration.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
